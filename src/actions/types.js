90930a3e015342980f8b1bb8502705a67f72471a
Gery Egan <gery.egan@gmail.com>
2017-03-07 00:12:38 -0800

    export types

---
+++ src/actions/types.js
@@ -0,0 +1,8 @@
+export const EMAIL_CHANGED = 'email_changed';
+export const PASSWORD_CHANGED = 'password_changed';
+export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGIN_USER_FAIL = 'login_user_fail';
+export const LOGIN_USER = 'login_user';
+export const TASK_UPDATE = 'task_update';
+export const TASK_CREATE = 'task_create';
+export const TASKS_FETCH_SUCCESS = 'tasks_fetch_success';
