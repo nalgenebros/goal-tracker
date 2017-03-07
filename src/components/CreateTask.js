14407f0ce3a7a5c6e6000f613c2be4a7d4dc22d7
Gery Egan <gery.egan@gmail.com>
2017-03-07 00:13:10 -0800

    CreateTask Component

---
+++ src/components/CreateTask.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View, Picker, Text } from 'react-native';
+import { Button } from './common';
+import { Input, Card, CardSection, } from './gridercommon';
+import { taskUpdate, taskCreate } from '../actions';
+
+class CreateTask extends Component {
+    onButtonPress() {
+        const { title, status } = this.props;
+        this.props.taskCreate({ title, status: status || 'Incomplete' });
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Card>
+                    <CardSection>
+                        <Input
+                        placeholderTextColor={'#FFF'}
+                        placeholder='Add a Task!' 
+                        onChangeText={value => this.props.taskUpdate({ prop: 'title', value })}
+                        label='Task Title'
+                        />
+                        {/*<Input 
+                        label='Task Name' 
+                        placeholder='Task Name' 
+                        onChangeText={value => this.props.taskUpdate({ prop: 'title', value })} 
+                        /> */}
+                    </CardSection>
+
+                    <CardSection >
+                        <Text style={styles.pickerLabelStyle}>Status</Text>
+                        <Picker 
+                        selectedValue={this.props.status}
+                        onValueChange={value => this.props.taskUpdate({ prop: 'status', value })}
+                        style={{ flex: 1 }}
+                        >
+                            <Picker.Item label='Incomplete' value='Incomplete' />
+                            <Picker.Item label='Complete' value='Complete' />
+                        </Picker>
+                    </CardSection>
+                    <CardSection>
+                        <Button onPress={this.onButtonPress.bind(this)}>
+                            Add Task
+                        </Button>
+                    </CardSection>
+                </Card>
+            </View>
+            
+        );
+    }
+}
+
+const styles = {
+    pickerLabelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        color: '#FFF'
+    },
+    container: {
+          flex: 1,
+          backgroundColor: '#4CAF50'
+    }
+};
+
+
+const mapStateToProps = (state) => {
+    const { title, status } = state.tasksForm;
+
+    return { title, status };
+};
+
+export default connect(mapStateToProps, { taskUpdate, taskCreate })(CreateTask);
