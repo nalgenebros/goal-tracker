847d7307ca0d914962de96aacfb0f98f3960ad7b
Gery Egan <gery.egan@gmail.com>
2017-03-14 18:58:33 -0700

    fixed initialstate bug

--- src/reducers/TasksReducer.js
+++ src/reducers/TasksReducer.js
@@ -2,7 +2,16 @@ import { TASK_UPDATE, TASK_CREATE, TASKS_FETCH_SUCCESS } from '../actions/types'
 
 const INITIAL_STATE = {
     title: '',
-    status: ''
+    status: '',
+    days: {
+      Monday: false,
+      Tuesday: false,
+      Wednesday: false,
+      Thursday: false,
+      Friday: false,
+      Saturday: false,
+      Sunday: false  
+    }
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -10,10 +19,10 @@ export default (state = INITIAL_STATE, action) => {
         case TASK_UPDATE:
             return { ...state, [action.payload.prop]: action.payload.value };
         case TASK_CREATE:
-            return { INITIAL_STATE };
+            return { ...INITIAL_STATE };
         case TASKS_FETCH_SUCCESS:
             return { ...state };
         default:
-            return state;
+            return { ...state };
     }
 };
